125684b6306db84e2436d24d7885f3398ccafff0
janki-7span <janki@7span.com>
2023-06-15 06:13:41 +0530

    design fix

--- script.js
+++ script.js
@@ -8,4 +8,47 @@ toggleBtn.onclick = () => {
 };
 // header over
 
+// faq start
+document.addEventListener('alpine:init', () => {
+      Alpine.store('accordion', {
+        tab: 0
+      });
+      
+      Alpine.data('accordion', (idx) => ({
+        init() {
+          this.idx = idx;
+        },
+        idx: -1,
+        handleClick() {
+          this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
+        },
+        handleRotate() {
+          return this.$store.accordion.tab === this.idx ? 'rotate-180' : '';
+        },
+        handleToggle() {
+          return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
+        }
+      }));
+    })
+// faq over
 
+// marquee tag
+module.exports = {
+  extend: {
+    animation: {
+      marquee: 'marquee 25s linear infinite',
+      marquee2: 'marquee2 25s linear infinite',
+    },
+    keyframes: {
+      marquee: {
+        '0%': { transform: 'translateX(0%)' },
+        '100%': { transform: 'translateX(-100%)' },
+      },
+      marquee2: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0%)' },
+      },
+    },
+  }
+}
+// marquee tag
