5aab2b3304d82f2265739a6db481d61434ab07a0
janki-7span <janki@7span.com>
2023-06-16 05:56:12 +0530

    changes

--- script.js
+++ script.js
@@ -33,22 +33,5 @@ document.addEventListener('alpine:init', () => {
 // faq over
 
 // marquee tag
-module.exports = {
-  extend: {
-    animation: {
-      marquee: 'marquee 25s linear infinite',
-      marquee2: 'marquee2 25s linear infinite',
-    },
-    keyframes: {
-      marquee: {
-        '0%': { transform: 'translateX(0%)' },
-        '100%': { transform: 'translateX(-100%)' },
-      },
-      marquee2: {
-        '0%': { transform: 'translateX(100%)' },
-        '100%': { transform: 'translateX(0%)' },
-      },
-    },
-  }
-}
+
 // marquee tag
